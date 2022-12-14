3a61796bc7607561d197dacc269d7d74a4d28050
Keaton Law <keaton.c.law@gmail.com>
2022-12-13 20:28:04 -0600

    postform component

--- components/PostForm.js
+++ components/PostForm.js
@@ -1,7 +1,83 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {
+  useState, useEffect,
+} from 'react';
+import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+import { Button, Form } from 'react-bootstrap';
+import { updatePost, createPost } from '../utils/data/postData';
 
-export default function PostForm() {
+const initialState = {
+  id: null,
+  title: '',
+  publicationDate: '',
+  imageUrl: '',
+  content: '',
+  approved: false,
+};
+
+const PostForm = ({ user, postObj }) => {
+  const [currentPost, setCurrentPost] = useState(initialState);
+  const router = useRouter;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCurrentPost((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (postObj.id) {
+      updatePost(user, currentPost, postObj.id).then(() => router.push('/posts'));
+    } else {
+      createPost(user, currentPost).then(() => router.push('/posts'));
+    }
+  };
+
+  const getAndSet = () => {
+    if (postObj.id) {
+      setCurrentPost(postObj);
+    }
+  };
+  useEffect(() => {
+    getAndSet();
+  }, [postObj]);
+
   return (
-    <div>PostForm</div>
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control name="title" required value={currentPost.title} onChange={handleChange} />
+          <Form.Label>Publication Date</Form.Label>
+          <Form.Control name="publication-date" type="date" required value={currentPost.publicationDate} onChange={handleChange} />
+          <Form.Label>Image</Form.Label>
+          <Form.Control name="image-url" required value={currentPost.imageUrl} onChange={handleChange} />
+          <Form.Label>Post Content</Form.Label>
+          <Form.Control name="content" required value={currentPost.content} onChange={handleChange} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
-}
+};
+
+PostForm.propTypes = {
+  user: PropTypes.shape({
+    uid: PropTypes.string,
+  }).isRequired,
+  postObj: PropTypes.shape({
+    id: PropTypes.number,
+  }),
+};
+
+PostForm.defaultProps = {
+  postObj: initialState,
+};
+
+export default PostForm;
