f03850d0e651d125f49b8581a7559e9b0d2af6d0
Keaton Law <keaton.c.law@gmail.com>
2022-12-17 13:11:28 -0600

    added api calls for userchange entity

---
+++ utils/data/userChangeData.js
@@ -0,0 +1,58 @@
+import { clientCredentials } from '../client';
+
+const getUserChanges = () => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/userchanges`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getUserChangeById = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/userchanges/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      resolve({
+        id: data.id,
+        action: data.action,
+        admin: data.admin,
+        secondAdmin: data.second_admin,
+        modifiedUser: data.modified_user,
+      });
+    })
+    .catch(reject);
+});
+
+const createUserChange = (user, userChange) => new Promise((resolve, reject) => {
+  const userChangeObj = {
+    action: userChange.action,
+    admin: user.uid,
+    second_admin: null,
+    modified_user: userChange.modifiedUser,
+  };
+  fetch(`${clientCredentials.databaseURL}/userchanges`, {
+    method: 'POST',
+    body: JSON.stringify(userChangeObj),
+    headers: {
+      Authorization: user.uid,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((resp) => resolve(resp.json()))
+    .catch((error) => reject(error));
+});
+
+const deletUserChange = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/userchanges/${id}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((response) => resolve(response))
+    .catch((error) => reject(error));
+});
+
+export {
+  getUserChanges,
+  getUserChangeById,
+  createUserChange,
+  deletUserChange,
+};
