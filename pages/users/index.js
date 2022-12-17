20be00612d1e40c7e49c5344a9415c5a4027dcba
Krima Patel <patel.krima@hotmail.com>
2022-12-17 10:15:26 -0500

    worked on view users and users index

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import RareUserCard from '../../components/RareUserCard';
+import { getRareUsers } from '../../utils/data/rareUserData';
 
 export default function Users() {
+  const [rareUsers, setRareUsers] = useState([]);
+
+  const getTheRareUsers = () => {
+    getRareUsers().then((data) => setRareUsers(data));
+  };
+
+  useEffect(() => {
+    getTheRareUsers();
+  }, []);
+
   return (
-    <div>Users</div>
+    <>
+      <article>
+        <h1>Rare Publishing Users</h1>
+        {rareUsers.map((rareUser) => (
+          <section key={`rareUser--${rareUser.id}`} className="rare-user">
+            <RareUserCard id={rareUser.id} first_name={rareUser.first_name} last_name={rareUser.last_name} email={rareUser.email} bio={rareUser.bio} active={rareUser.active} />
+          </section>
+        ))}
+      </article>
+    </>
   );
 }
