0b891d9ec62993c0ccb6b2efa9f101bf066f880a
Krima Patel <patel.krima@hotmail.com>
2022-12-17 19:14:48 -0500

    commenting out active user api calls for now

--- utils/data/rareUserData.js
+++ utils/data/rareUserData.js
@@ -7,4 +7,24 @@ const getRareUsers = () => new Promise((resolve, reject) => {
   .catch(reject);
 });
 
+// const isActive = (id) => new Promise((resolve, reject) => {
+//   fetch(`${clientCredentials.databaseURL}/users/${id}/active`, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ id }),
+//   })
+//     .then((response) => resolve(response))
+//     .catch((error) => reject(error));
+// });
+
+// const isNotActive = (id) => new Promise((resolve, reject) => {
+//   fetch(`${clientCredentials.databaseURL}/users/${id}/notactive`, {
+//     method: 'DELETE',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ id }),
+//   })
+//     .then((response) => resolve(response))
+//     .catch((error) => reject(error));
+// });
+
 export { getRareUsers };
