a3f510cee5ae5cfc4e838440a9a87e06cb1c835f
Keaton Law <keaton.c.law@gmail.com>
2022-12-21 20:47:23 -0600

    completed logic for user changes

--- components/UserChangeCard.js
+++ components/UserChangeCard.js
@@ -1,29 +1,50 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
-
+import { useAuth } from '../utils/context/authContext';
 import { deleteUserChange } from '../utils/data/userChangeData';
+import { userActiveChange, userStaffChange } from '../utils/data/rareUserData';
+import { useRouter } from 'next/router';
 
 export default function UserChangeCard({
-  uChangeObj, onUpdate,
+  id, action, modified_userLN, modified_userFN, modified_userID, admin, onUpdate,
 }) {
+  const router = useRouter();
+  const { user } = useAuth();
+  const approveThisAction = () => {
+    if (action === 'promote' || action === 'demote') {
+      userStaffChange(modified_userID, user.uid).then(deleteUserChange(id).then(() => router.push('/users')));
+    } else if (action === 'activate' || action === 'deactivate') {
+      userActiveChange(modified_userID, user.uid).then(deleteUserChange(id).then(() => router.push('/users')));
+    } else if (action === 'promoteAndActivate' || action === 'demoteAndDeactivate') {
+      userStaffChange(modified_userID, user.uid).then(() => (userActiveChange(modified_userID, user.uid))).then(deleteUserChange(id).then(() => router.push('/users')));
+    }
+  };
   const deleteThisUChange = () => {
-    if (window.confirm(`Delete ${uChangeObj.action}?`)) {
-      deleteUserChange(uChangeObj.id).then(() => onUpdate());
+    if (window.confirm(`Delete ${action}?`)) {
+      deleteUserChange(id).then(() => onUpdate());
       window.location.reload();
     }
   };
+
   return (
     <>
       <Card className="text-center">
         <Card.Body>
-          <Card.Title>{uChangeObj.action}</Card.Title>
-          <Card.Text>modified user name</Card.Text>
-          <Card.Text>admin name</Card.Text>
+          <Card.Title>{action}</Card.Title>
+          <Card.Text>{modified_userFN} {modified_userLN}</Card.Text>
           <Button variant="danger" onClick={deleteThisUChange} className="m-2">
             DELETE
           </Button>
+          {
+            user.uid !== admin ? (
+              <Button variant="danger" onClick={approveThisAction} className="m-2">
+                APPROVE
+              </Button>
+            ) : <>This action is pending approval</>
+          }
         </Card.Body>
         <Card.Footer />
       </Card>
@@ -32,21 +53,37 @@ export default function UserChangeCard({
 }
 
 UserChangeCard.propTypes = {
-  uChangeObj: PropTypes.shape({
-    id: PropTypes.number,
-    action: PropTypes.string,
-    admin: PropTypes.shape({
-      id: PropTypes.number,
-      first_name: PropTypes.string,
-      last_name: PropTypes.string,
-      uid: PropTypes.string,
-    }).isRequired,
-    modifiedUser: PropTypes.shape({
-      id: PropTypes.number,
-      first_name: PropTypes.string,
-      last_name: PropTypes.string,
-      uid: PropTypes.string,
-    }).isRequired,
-  }).isRequired,
+  id: PropTypes.number.isRequired,
+  action: PropTypes.string.isRequired,
+  admin: PropTypes.string.isRequired,
+  modified_userFN: PropTypes.string.isRequired,
+  modified_userLN: PropTypes.string.isRequired,
+  modified_userID: PropTypes.number.isRequired,
   onUpdate: PropTypes.func.isRequired,
 };
+
+// UserChangeCard.propTypes = {
+//   changeObj: PropTypes.shape({
+//     id: PropTypes.number,
+//     action: PropTypes.string,
+//     admin: PropTypes.shape({
+//       id: PropTypes.number,
+//       first_name: PropTypes.string,
+//       last_name: PropTypes.string,
+//       uid: PropTypes.string,
+//     }).isRequired,
+//     second_admin: PropTypes.shape({
+//       id: PropTypes.number,
+//       first_name: PropTypes.string,
+//       last_name: PropTypes.string,
+//       uid: PropTypes.string,
+//     }).isRequired,
+//     modified_user: PropTypes.shape({
+//       id: PropTypes.number,
+//       first_name: PropTypes.string,
+//       last_name: PropTypes.string,
+//       uid: PropTypes.string,
+//     }).isRequired,
+//   }).isRequired,
+//   onUpdate: PropTypes.func.isRequired,
+// };
--- pages/userchange/index.js
+++ pages/userchange/index.js
@@ -1,14 +1,15 @@
-
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { getUserChanges } from '../../utils/data/userChangeData';
+import { useAuth } from '../../utils/context/authContext';
 import UserChangeCard from '../../components/UserChangeCard';
 
 function Home() {
+  const user = useAuth();
   const [uChanges, setUChanges] = useState([]);
 
   useEffect(() => {
-    getUserChanges().then((setUChanges));
+    getUserChanges(user.uid).then((setUChanges));
   }, []);
 
   return (
@@ -21,8 +22,10 @@ function Home() {
             <UserChangeCard
               id={uChange.id}
               action={uChange.action}
-              admin={uChange.admin}
-              modifiedUser={uChange.modifiedUser}
+              admin={uChange.admin.uid}
+              modified_userFN={uChange.modified_user.first_name}
+              modified_userLN={uChange.modified_user.last_name}
+              modified_userID={uChange.modified_user.id}
               onUpdate={getUserChanges}
             />
           </section>
